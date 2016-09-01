74b2b3e826914478d49b8a243db5ae6e1edf8f53
Michael Jung <michaeljung@s172-20-106-h87.paws.uga.edu>
2016-08-31 21:04:35 -0400

    Pipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    selector: 'async-example',
+    template: `<div>
+    	<p>Wait for it... {{ greeting | async }}</p>
+    	<button (click)="clicked()">{{ arrived ? 'Reset' : 'Resolve' }}</button>
+    	</div>`
 })
-export class AppComponent { }
+export class AppComponent { 
+	greeting: Promise<string> = null;
+	arrived: boolean = false;
+
+	private resolve: Function = null;
+
+	constructor() { this.reset(); }
+
+	reset() {
+		this.arrived = false;
+		this.greeting = new Promise<string>((resolve, reject) => { this.resolve = resolve; });
+	}
+
+	clicked() {
+		if (this.arrived) {
+			this.reset();
+		} else {
+			this.resolve('hi there!');
+			this.arrived = true;
+		}
+	}
+
+
+
+
+}
