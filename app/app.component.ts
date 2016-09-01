a36cd4fb79655c8d42f1070bc9ff0de292b1b1ad
Michael Jung <michaeljung@s172-20-106-h87.paws.uga.edu>
2016-08-31 21:29:42 -0400

    DatePipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'async-example',
     template: `<div>
-    	<p>A: {{a | currency:'EUR':true}}</p>
-    	<p>B: {{b | currency:'USD':true:'4.2-2'}}</p>
-    </div>	
+    	<p>Today is {{today | date}}</p>
+    	<p>Or if you prefer, {{today | date:'fullDate'}}</p>
+    	<p>The time is {{today | date:'jmZ'}}<p>
+    	<div>
     `
 })
 export class AppComponent { 
-	a: number = 0.259;
-	b: number = 1.3495;
+	today: number = Date.now();
 
 }
