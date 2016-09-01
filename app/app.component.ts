ec11a6ea56d338730bf0b9ff215740e82869e35e
Michael Jung <michaeljung@s172-20-106-h87.paws.uga.edu>
2016-08-31 21:21:50 -0400

    CurrencyThingh

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,33 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'async-example',
     template: `<div>
-    	<p>Wait for it... {{ greeting | async }}</p>
-    	<button (click)="clicked()">{{ arrived ? 'Reset' : 'Resolve' }}</button>
-    	</div>`
+    	<p>A: {{a | currency:'EUR':true}}</p>
+    	<p>B: {{b | currency:'USD':true:'4.2-2'}}</p>
+    </div>	
+    `
 })
 export class AppComponent { 
-	greeting: Promise<string> = null;
-	arrived: boolean = false;
-
-	private resolve: Function = null;
-
-	constructor() { this.reset(); }
-
-	reset() {
-		this.arrived = false;
-		this.greeting = new Promise<string>((resolve, reject) => { this.resolve = resolve; });
-	}
-
-	clicked() {
-		if (this.arrived) {
-			this.reset();
-		} else {
-			this.resolve('hi there!');
-			this.arrived = true;
-		}
-	}
-
-
-
+	a: number = 0.259;
+	b: number = 1.3495;
 
 }
