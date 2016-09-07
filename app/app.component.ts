b3a712fd8b678c1dbb41f21623ebaa7893c1f8b9
Michael Jung <michaeljung@s172-21-15-h188.paws.uga.edu>
2016-09-06 20:30:56 -0400

    PercentPipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
+import { NgSwitch } from '@angular/common';
+import { NgSwitchCase } from '@angular/common';
+import { NgSwitchDefault } from '@angular/common';
 
 @Component({
     selector: 'async-example',
-    template: `<div>
-    	<p>Without JSON pipe:</p>
-    	<pre>{{object}}</pre>
-    	<p>With JSON pipe:</p>
-    	<pre>{{object | json}}</pre>
-
-
-    	<div>
-    `
+    template: `
+    <div>
+    	<p>A: {{a | percent}}</p>
+    	<p>B: {{b | percent:'1.4-5'}}</p>
+    </div>
+  `
 })
 export class AppComponent { 
-	object: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
+  a: number = 0.259;
+  b: number = 1.3495;
 
 }
